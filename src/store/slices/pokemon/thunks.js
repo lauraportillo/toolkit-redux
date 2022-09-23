27778d04fef523d0d6afed43b07e9913ae9537fb
Laura Portillo <lauraportillo.ark@gmail.com>
2022-09-23 13:07:18 +0200

    add endpoint

--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -6,7 +6,10 @@ export const getPokemons = (page = 0) => {
         dispatch(startLoadingPokemons());
 
         // TODO: realizar petición http
-        // dataa-pokemons
+
+        const response = await fetch(` https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`);
+        const data = await response.json();
+        console.log(data);
 
         // dispatch(setPokemons());
 
